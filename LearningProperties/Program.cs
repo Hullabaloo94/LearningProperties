0ea91123d41679c33e4f435775079a45f6dd1153
Jason Hall <jasonahall1994@gmail.com>
2019-11-14 12:36:30 +0000

    initial commit

---
+++ LearningProperties/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LearningProperties
+{
+    class Player
+    {
+        public bool IsAlive 
+        {
+            get { return health > 0; }
+        }
+
+        int health = 100;
+
+        public void Hit()
+        {
+            Random r = new Random();
+            health -= r.Next(5, 50);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            for (int i = 0; i < 20; i++)
+            {
+                player.Hit();
+                Console.WriteLine("Is player alive: " + player.IsAlive);
+            }
+        }
+    }
+}
